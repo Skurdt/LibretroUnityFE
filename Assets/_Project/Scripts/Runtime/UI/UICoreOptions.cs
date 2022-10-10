6dbd7f65543c399ca0221871d6c2abf0d64c9e31
Skurdt <skurdt@gmail.com>
2022-10-11 00:12:41 +0200

    Nothing to see

--- Assets/_Project/Scripts/Runtime/UI/UICoreOptions.cs
+++ Assets/_Project/Scripts/Runtime/UI/UICoreOptions.cs
@@ -20,7 +20,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE. */
 
-using SK.Libretro;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
